c153c51e44454f49223c34227bad8468aaabbb27
huysmanski <huysmanski@31e67e5e-c560-11de-b12b-e93134c88cf6>
2009-11-13 22:00:32 +0000

    add First PopupMenu for removing client

--- trunk/ProjectorControllerServer/src/be/rhea/projector/controller/server/ui/BeanEditor.java
+++ trunk/ProjectorControllerServer/src/be/rhea/projector/controller/server/ui/BeanEditor.java
@@ -17,6 +17,7 @@ public class BeanEditor extends JPanel {
 		this.setLayout(new BorderLayout());
 		this.setBounds(10, 10, 300, 300);
 		table = new PropertyTable();
+		table.putClientProperty("terminateEditOnFocusLost", Boolean.TRUE);
 		JScrollPane ps = new JScrollPane();
 		ps.getViewport().add(table);
 		this.add(ps, BorderLayout.CENTER);
@@ -26,5 +27,9 @@ public class BeanEditor extends JPanel {
 	public void setObject(Object bean, ArrayList<Client> clients) {
 		data = new AnnotationPropertyTableData(bean, clients);
 		table.setPropertyTableData(data);
+	}
+
+	public PropertyTable getTable() {
+		return table;
 	}
 }
