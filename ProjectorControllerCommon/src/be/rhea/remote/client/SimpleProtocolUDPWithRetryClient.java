b90bdc73723c4726a2ae6d334c5275c5ac7481bf
huysmanski <huysmanski@31e67e5e-c560-11de-b12b-e93134c88cf6>
2009-11-30 20:24:50 +0000

    Added retry send with UDP protocol

---
+++ ProjectorControllerCommon/src/be/rhea/remote/client/SimpleProtocolUDPWithRetryClient.java
@@ -0,0 +1,78 @@
+package be.rhea.remote.client;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+
+public class SimpleProtocolUDPWithRetryClient extends SimpleProtocolClient {
+
+	private int port;
+	private String host;
+	private final String protocol;
+	private static final int SLEEP_TIME_BETWEEN_SEND_REQUESTS = 20;
+
+	public SimpleProtocolUDPWithRetryClient(String host, int port, String protocol) {
+		this.port = port;
+		this.host = host;
+		this.protocol = protocol;
+	}
+
+	@Override
+	public void connect() throws IOException {
+	}
+
+	@Override
+	public String sendCommand(String command) throws IOException {
+		sendData(protocol + ":" + command);
+		
+		return "OK";
+	}
+	
+    	
+
+	private void sendData(String data) throws SocketException,
+			UnknownHostException, IOException {
+		DatagramSocket datagramSocket = new DatagramSocket();
+		InetAddress a = InetAddress.getByName(host);
+	    byte[] inetaddress = a.getAddress();
+	    
+		DatagramPacket packet = new DatagramPacket(data.getBytes(), data.length(), InetAddress.getByAddress(inetaddress), port);
+		datagramSocket.send(packet);
+		try {
+			Thread.sleep(SLEEP_TIME_BETWEEN_SEND_REQUESTS);
+		} catch (InterruptedException e) {
+		}
+		datagramSocket.send(packet);
+	}
+
+	@Override
+	public String sendCommand(String command, String[] parameter) throws IOException {
+		StringBuilder commandString = new StringBuilder();
+		commandString.append(protocol);
+		commandString.append(":");
+		commandString.append(command);
+		if (parameter != null) {
+			for (int i = 0; i < parameter.length; i++) {
+				commandString.append(" ");
+				commandString.append(parameter[i]);
+				
+			}
+		}
+		sendData(commandString.toString());
+
+		return "OK";
+	}	
+	
+	@Override
+	public String executeStreamingCommand(String command, SimpleProtocolClientStreamingCommand clientCommand) throws IOException {
+		return null;
+	}
+
+	@Override
+	public void disconnect() throws IOException {
+	}
+}
---
+++ ProjectorControllerCommon/src/be/rhea/remote/server/SimpleProtocolUDPWithRetryServer.java
@@ -0,0 +1,103 @@
+package be.rhea.remote.server;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.util.Arrays;
+import java.util.logging.Level;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+public class SimpleProtocolUDPWithRetryServer extends SimpleProtocolServer implements Runnable {
+
+	private int port;
+	private Thread serverThread;
+	private boolean running = true;
+	private DatagramSocket datagramSocket;
+	private final String protocol;
+
+	public SimpleProtocolUDPWithRetryServer(int port, String protocol) {
+		this.port = port;
+		this.protocol = protocol;
+	}
+
+	public void start() throws IOException {
+		datagramSocket = new DatagramSocket(port);
+		datagramSocket.setSoTimeout(0);
+	
+		serverThread = new Thread(this);
+		serverThread.start();
+	}
+
+	public void stop() throws IOException {
+		running = false;
+		serverThread = null;
+		datagramSocket.close();
+	}
+	
+	public void run() {
+		byte[] oldPacket = null;
+		try {
+			while (running) {
+				byte[] buffer = new byte[1024];
+				DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+				datagramSocket.receive(packet);
+
+				if (oldPacket == null || !Arrays.equals(packet.getData(), oldPacket)) {
+					Thread sockethandletThread = new Thread(new SocketHandler(
+							packet));
+					sockethandletThread.start();
+				}
+				oldPacket = packet.getData();
+
+			}
+		} catch (IOException e) {
+			if (running) {
+				logger.log(Level.SEVERE,
+						   "Exception occurred when starting SimpleProtocolServer",
+						   e);
+				throw new RuntimeException(e);
+			}
+		}
+
+	}
+
+	private class SocketHandler implements Runnable {
+
+		private final DatagramPacket packet;
+
+		public SocketHandler(DatagramPacket packet) {
+			this.packet = packet;
+		}
+
+		public void run() {
+			try {
+				String line = new String(packet.getData(), 0, packet.getLength());
+				//System.out.println("Data = " + line);
+				String[] lineParts = line.split(" ");
+				//System.out.println("CommandName = " + lineParts[0]);
+				SimpleProtocolServerCommand command = (SimpleProtocolServerCommand) commandMap
+						.get(lineParts[0]);
+				String[] parameters = null;
+				if (lineParts.length > 1) {
+					parameters = new String[lineParts.length - 1];
+					System.arraycopy(lineParts, 1, parameters, 0, lineParts.length - 1);
+				}
+				 
+				//System.out.println("Command = " + command);
+				if (command != null) {
+					if (command instanceof SimpleProtocolServerExecuteCommand) {
+						((SimpleProtocolServerExecuteCommand) command).execute(parameters);
+					} else if (command instanceof SimpleProtocolServerStreamingCommand) {
+						throw new NotImplementedException();
+					}
+				}
+			} catch (Throwable t) {
+				logger.log(Level.SEVERE,
+						"Exception occurred in SimpleProtocolServer", t);
+			}
+
+		}
+	}
+
+}
